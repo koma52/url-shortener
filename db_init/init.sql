d96197bbd24948301e8f83d5d1cfc49fb82da730
Márton Kónya <konyamarci52@proton.me>
2023-07-21 23:30:34 +0200

    chenged db schema

--- db_init/init.sql
+++ db_init/init.sql
@@ -1,10 +1,13 @@
 CREATE DATABASE IF NOT EXISTS urlshortener;
 use urlshortener;
 CREATE TABLE `shortenedurls` (
-	  `shortcode` int(12) unsigned NOT NULL auto_increment,
+	  `id` int(12) unsigned NOT NULL AUTO_INCREMENT,
+	  `shortcode` char(6) NOT NULL,
 	  `longurl` varchar(255) NOT NULL,
 	  `active` bool,
 	  `created` TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-	  PRIMARY KEY  (`shortcode`),
+	  
+	  PRIMARY KEY  (`id`),
 	  UNIQUE KEY `long` (`longurl`)
+	  UNIQUE KEY `short` (`shortcode`)
 );
